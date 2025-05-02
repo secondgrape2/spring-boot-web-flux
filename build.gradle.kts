b2ea687740b6fcdde30dc069dbdf89b68ccbd27c
inkyu <87048860+grapestore@users.noreply.github.com>
2025-05-02 17:56:10 +0900

    feat: init sprint boot webflux (#1)

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.2.3"
+	id("io.spring.dependency-management") version "1.1.4"
+	kotlin("jvm") version "1.9.22"
+	kotlin("plugin.spring") version "1.9.22"
+}
+
+group = "com.mycompany"
+version = "0.0.1-beta0.1"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+configurations.all {
+	resolutionStrategy {
+		failOnVersionConflict()
+		enableDependencyVerification()
+	}
+}
+
+dependencyLocking {
+	lockAllConfigurations()
+}
+
+dependencies {
+	constraints {
+        implementation("org.jetbrains:annotations:23.0.0") {
+            because("kotlinx-coroutines requires 23.0.0, kotlin-stdlib requires 13.0, resolving conflict")
+        }
+    }
+		
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation(kotlin("reflect"))
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testImplementation(kotlin("test-junit5"))
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "17"
+	}
+}
+
+tasks.test {
+	useJUnitPlatform()
+}
